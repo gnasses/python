014ba7043166b26fa379909bf0565171924ed5a7
gnasses <35275315+gnasses@users.noreply.github.com>
2020-02-26 13:30:23 -0600

    Update bgp_asn_check.py

--- bgp_asn_check.py
+++ bgp_asn_check.py
@@ -1,46 +1,69 @@
-"""
-Created on Tue Feb 25 16:46:37 2020
-
-@author: gnasses
-"""
-input_as = '<ASN>'
-import util
+#!/usr/bin/python3
+from __future__ import print_function, unicode_literals
 from netmiko import Netmiko
+import util
 from ntc_templates.parse import parse_output
-devices = [dc1-core-sw, 'dc2-core-sw', 'dc3-core-sw]
-aslist = []
-for dev in devices:
-    device = util.CiscoDeviceRO(host=dev)
-    dev_connect = Netmiko(**device.__dict__)
-    bgp_out = dev_connect.send_command("show ip bgp vrf all")
-    bgp_parsed = parse_output(platform="cisco_nxos", command="show ip bgp", data=bgp_out)
-    dev_connect.disconnect()
-    print ("Collected BGP table for " + dev)
-    bgpasns = [ sub['as_path'] for sub in bgp_parsed ]
-    for asn in bgpasns:
-        asnsplit = asn.split()
-        for asnum in asnsplit:
-            if asnum not in aslist and int(asnum) >=64512 and int(asnum) <=65535:
-                aslist.append(asnum)
-as_map = map(int, aslist)
-as_sort = sorted(as_map)
-print ("Requested ASN Number: " + input_as)
-if input_as in aslist:
-    print ("AS **NOT** AVAIALABLE")
-else:
-    print ("AS is availble")
-avail = 64512
-avail_count = 5
-print ("Next 5 available Private ASNs from range 64512-65535")
-while avail_count > 0:
-    if str(avail) not in aslist:
-        print (avail)
-        avail = avail +1
-        avail_count = avail_count -1
+import cgi
+import cgitb
+cgitb.enable()
+form = cgi.FieldStorage()
+input_as = form.getfirst('input_as')
+print("Content-Type: text/html\n\n")
+print('''
+<style>
+table {
+  border-collapse: collapse;
+}
+td, th {
+  border: 1px solid black;
+  text-align: left;
+  padding: 8px;
+}
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>''')
+if int(input_as) >= 64512 and int(input_as) <= 65535:
+    devices = ['dc1-core-sw', 'dc2-core-sw', 'dc3-core-sw']
+    aslist = []
+    print ("<h2> Live ASN Check </h2>")
+    for dev in devices:
+        device = util.CiscoDeviceRO(host=dev)
+        dev_connect = Netmiko(**device.__dict__)
+        bgp_out = dev_connect.send_command("show ip bgp vrf all")
+        bgp_parsed = parse_output(platform="cisco_nxos", command="show ip bgp", data=bgp_out)
+        dev_connect.disconnect()
+        print ("Collected BGP table for " + dev)
+        print ('<br />') 
+        bgpasns = [ sub['as_path'] for sub in bgp_parsed ]
+        for asn in bgpasns:
+            asnsplit = asn.split()
+            for asnum in asnsplit:
+                if asnum not in aslist and int(asnum) >=64512 and int(asnum) <=65535:
+                    aslist.append(asnum)
+    as_map = map(int, aslist)
+    as_sort = sorted(as_map)
+    print ("<h4> Requested ASN Number: " + str(input_as) + "</h4>")
+    if input_as in aslist:
+        print ('<p style="color:red;">AS in use and <b>**NOT**</b> AVAILABLE</p>')
     else:
-        avail = avail +1       
-ascount = str(len(aslist))
-print ("Number of unique Private ASNs across all Devices: " + ascount)
-print ("Ordered List of AS in use: ")
-print(as_sort)
-print ()
+        print ('<p style="color:green;">AS is currently <b>unused</b></p>')
+    print ('<br />')     
+    print ('<br />')  
+    avail = 64512
+    avail_count = 5
+    print ("<table><caption><b>Next 5 available Private ASNs from range 64512-65535</b></caption><tr>")
+    while avail_count > 0:
+        if str(avail) not in aslist:
+            print ("<td>" + str(avail) + "</td>")
+            avail = avail +1
+            avail_count = avail_count -1
+        else:
+            avail = avail +1       
+    print ("</tr></table></br />")
+    ascount = str(len(aslist))
+    print ('<b>Number of unique Private ASNs across all Devices: " + ascount + "</b><br /><br />')
+    print ("Ordered List of AS in use: <br />")
+    print(as_sort)
+else: 
+    print ("UNABLE TO VALIDATE: AS Entered was outside of Valid Private Range (64512-65535)")
